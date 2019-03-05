933a4a2fa66050f09235441de50eec7a0ecb7173
Ulgerd <melky@li.ru>
2019-03-05 16:06:20 +0300

    SignUp simplified

--- SignUp.js
+++ SignUp.js
@@ -5,9 +5,6 @@ class Form extends Component {
     super(props);
     this.state = {
       errors: {
-        usernameError: '',
-        emailError: '',
-        checkError: ''
       },
       userData: {
         'username': '',
@@ -18,87 +15,102 @@ class Form extends Component {
   }
 
   onSubmit = (e) => {
-    let prevData = {...this.state};
-
-    let usEr = (!prevData.userData.username) ?
-      'Username must be filled' : null;
-
-    let emEr = (!prevData.userData.email) ?
-      'Email must be filled' :
-      (!prevData.userData.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) ?
-      'Incorrect email': null;
-
-    let chEr = (!prevData.userData.checked) ?
-    'Agree must be checked' : null;
-
-    prevData.errors = {usernameError: usEr||'',
-                       emailError: emEr||'',
-                       checkError: chEr||''
-                      }
-
-    if (usEr===null && emEr===null && chEr===null) {
-      alert(JSON.stringify(prevData.userData))
-      prevData.userData = {
-        'username': '',
-        'email': '',
-        'checked': false
-      }
+    let {userData, errors}=this.state;
+    errors = {
+      usernameError: !userData.username ?
+        'Username must be filled' : null,
+      emailError: !userData.email ?
+        'Email must be filled' :
+        (!userData.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) ?
+          'Incorrect email': null,
+      checkError: !userData.checked ?
+        'Agree must be checked' : null
     }
-    this.setState(prevData);
-    e.preventDefault();
+
+    if (errors.usernameError===null &&
+        errors.emailError===null &&
+        errors.checkError===null) {
+          alert(JSON.stringify(userData))
+          userData = {
+            'username': '',
+            'email': '',
+            'checked': false
+          }
+        }
+    this.setState({
+      errors:{...errors},
+      userData:{...userData}});
   }
 
-  onInputChange = (event) => {
-    let prevData = {...this.state.userData};
-    prevData[event.target.name] = event.target.value;
-    this.setState(state => ({
-      ...state, userData: {...prevData }}))
-  }
+  onInputChange = (name, value) => {
+    let {userData} = this.state;
+    this.setState({
+      userData: {
+        ...userData,
+        [name]: value }})
+      }
 
   onCheckboxChange = () => {
-    this.setState(state => ({
-      ...state,
-      userData: {...state.userData, checked: !state.userData.checked }}))
+    let {userData} = this.state;
+    this.setState({
+      userData: {
+        ...userData,
+        checked: !userData.checked }})
   }
 
   render() {
+    let {userData, errors} = this.state
     return(
       <div>
-        <form onSubmit={this.onSubmit}>
-          <div>
-            <label> Username
-              <input
-              type="text"
-              name="username"
-              value={this.state.userData.username}
-              onChange={this.onInputChange}/>
-            </label>
-            <span style={{color: 'red'}}>{this.state.errors.usernameError}</span>
-          </div>
+        <div>
+          <label> Username
+            <input
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={e => this.onInputChange(
+              e.target.name,
+              e.target.value)}/>
+          </label>
+          <span
+            style={{color: 'red'}}>{errors.usernameError}
+          </span>
+        </div>
 
-          <div>
-            <label> Email
-              <input
+        <div>
+          <label> Email
+            <input
               type="text"
               name="email"
-              value={this.state.userData.email}
-              onChange={this.onInputChange}/>
-            </label>
-            <span style={{color: 'red'}} >{this.state.errors.emailError}</span>
-          </div>
+              value={userData.email}
+              onChange={e => this.onInputChange(
+                e.target.name,
+                e.target.value)}/>
+          </label>
+          <span
+            style={{color: 'red'}}>{errors.emailError}
+          </span>
+        </div>
 
-          <div>
-            <label>
-              <input
+        <div>
+          <label>
+            <input
               type="checkbox"
-              checked={this.state.userData.checked}
+              checked={userData.checked}
               onChange={this.onCheckboxChange} />
-              <span>I agree to the terms</span>
-            </label>
-            <span style={{color: 'red'}} >{this.state.errors.checkError}</span>
-          </div>
-          <p><input type="submit" value="Submit" /></p>
-        </form>
+            <span>I agree to the terms</span>
+          </label>
+          <span
+            style={{color: 'red'}} >{errors.checkError}
+          </span>
+        </div>
+        <p>
+          <button
+              type="button"
+              value="Submit"
+              onClick={this.onSubmit}>Submit
+          </button>
+        </p>
       </div>
     )
   }
